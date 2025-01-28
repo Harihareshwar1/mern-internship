c978aa0733d89f12629a02c765fab3a7688f04b3
harihareshwar <harihareshwar08@gmail.com>
2025-01-28 15:17:23 +0530

    added

--- src/ProjectCard.jsx
+++ src/ProjectCard.jsx
@@ -1,21 +1,21 @@
 /* eslint-disable react/prop-types */
 import './ProjectCard.css'
+
 const ProjectCard = (props) => {
   const toolsString = props.tools.join(" | ");
-  return ( <>
+  return (
+     <>
     <div className='Main' >
       <div className='project-card'>
           <div className='top'> <h2>{props.name.toUpperCase()}</h2>
           <h2>{props.year}</h2></div>
        <div>
-        
+
         <p>{props.des}
         </p> </div>
         <div className='bottom'>
           <h2>Tools : {toolsString}</h2>
-          
-
-          </div>
+      </div>
       </div>
     </div>
     </>
